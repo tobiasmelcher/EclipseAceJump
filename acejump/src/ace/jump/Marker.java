e0307f56a96e45a89a04c1a7b73cbc433dee9dc3
whunmr <whunmr@gmail.com>
2016-08-25 18:10:23 +0800

    add secondary jump markers. make it more like more like avy-jump.

---
+++ acejump/src/ace/jump/Marker.java
@@ -0,0 +1,37 @@
+package ace.jump;
+
+import java.util.ArrayList;
+
+public class Marker {
+    private String _marker;
+    private ArrayList<Integer> _offsets = new ArrayList<Integer>();
+
+    public Marker(String marker, int offset) {
+        _marker = marker;
+        addOffsetToMarker(offset);
+    }
+
+    public void addOffsetToMarker(int offset) {
+        _offsets.add(offset);
+    }
+
+    public ArrayList<Integer> getOffsets() {
+        return _offsets;
+    }
+
+    public char getMarkerChar() {
+        return _marker.charAt(0);
+    }
+
+    public String getMarker() {
+        return _marker;
+    }
+
+    public int getOffset() {
+        return _offsets.get(0);
+    }
+
+    public boolean isMappingToMultipleOffset() {
+        return _offsets.size() > 1;
+    }
+}
